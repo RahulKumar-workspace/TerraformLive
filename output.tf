aaeb07899c7cfe656a90132713ee02b8dfd428a0
Rahul Kumar <rahulkumar.workspace@gmail.com>
2026-07-19 16:20:54 +0530

    Configuring output.tf to show the output when we use 'for_each' as the meta argument instead of 'count'

--- output.tf
+++ output.tf
@@ -1,3 +1,5 @@
+# This is how we write Output when we use 'Count' as meta argument:-
+/*
 output "ec2_public_ip" {
   value = aws_instance.my_instance[*].public_ip
 }
@@ -9,8 +11,18 @@ output "ec2_public_dns" {
 output "ec2_private_ip" {
   value = aws_instance.my_instance[*].private_ip
 }
-
+*/
 /* Note:-
     We are creating multiple instances using "count" so we have to use "[*]" while providing the output.
     [*] just means that there are multiple resources of this type. So give me the output accordingly.
 */
+
+
+
+# And This is how we write Output when we use 'for_each' as meta argument:-
+
+output "ec2_public_ip" {
+  value = [
+    for key in aws_instance.my_instance : key.public_ip
+  ]
+}
